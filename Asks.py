110e48752e0c9c2751b8145a2ee6776297e46252
bobbypine <45605113+bobbypine@users.noreply.github.com>
2019-04-14 09:47:33 -0400

    Update Asks.py
    Added error handling for bad keywords.

--- Asks.py
+++ Asks.py
@@ -3,22 +3,26 @@ from stockxsdk import Stockx
 stockx = Stockx()
 
 while True:
-    item = input('What product?:')
-    if item == "":
-        print("Program Ended")
-        break
-    print()
-    product_id = stockx.get_first_product_id('{}'.format(item))
+    try:
+        item = input('What product?:')
+        if item == "":
+            print("Program Ended")
+            break
+        print()
+        product_id = stockx.get_first_product_id('{}'.format(item))
 
-    product = stockx.get_product(product_id)
+        product = stockx.get_product(product_id)
 
-    size = input("What size?:")
-    print()
-    asks = stockx.get_asks(product_id)
-    print("{} Size {}".format(product.title, size))
-    print()
-    for ask in asks:
-        if ask.shoe_size == "{}".format(size):
-            print("Size: {} ${} {} Ask(s)".format(ask.shoe_size, ask.order_price, ask.num_orders))
-            print('--------------------------')
-    print()
+        size = input("What size?:")
+        print()
+        asks = stockx.get_asks(product_id)
+        print("{} Size {}".format(product.title, size))
+        print()
+        for ask in asks:
+            if ask.shoe_size == "{}".format(size):
+                print("Size: {} ${} {} ask(s)".format(ask.shoe_size, ask.order_price, ask.num_orders))
+                print('--------------------------')
+        print()
+    except:
+        print('No Results for {}'.format(item.title()))
+        print()
